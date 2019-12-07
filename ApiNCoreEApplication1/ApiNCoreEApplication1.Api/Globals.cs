b1f3a5eafe0acadb3c418c6b832b15c463621622
Mikesb74 <andersonhewardt@gmail.com>
2019-12-06 23:00:05 -0700

    Add project files.

---
+++ ApiNCoreEApplication1/ApiNCoreEApplication1.Api/Globals.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiNCoreEApplication1.Api
+{
+    public static class Globals
+    {
+    }
+}
